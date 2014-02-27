973c04840ff808150f805b350a4177e35ced5dbd
evfrancis <evfrancis@users.noreply.github.com>
2014-02-27 04:19:24 -0500

    Update stack.c
    Comments for clarity

--- c/stack.c
+++ c/stack.c
@@ -19,8 +19,11 @@ int stack_delete(stack* my_stack) {
         return NULL_STACK;
 
     while (my_stack->head != NULL) {
+        // Traverse structure
         stack_ele* temp = my_stack->head;
         my_stack->head = my_stack->head->next;
+        
+        // Free both data and stack element
         free(temp->value);
         free(temp);
     }
@@ -37,6 +40,7 @@ int get_stack_size(stack* my_stack) {
 
     stack_ele* head = my_stack->head;
     while (head != NULL) {
+        // Traverse and count
         size++;
         head = head->next;
     }
@@ -54,17 +58,21 @@ int stack_push(stack* my_stack, void* data, int size) {
     if (size <= 0)
         return INVALID_SIZE;
 
+    // Create a new stack element
     stack_ele* newItem = (stack_ele*)malloc(sizeof(stack_ele));
 
     if (newItem == NULL)
         return OUT_OF_MEM;
 
+    // Initialize data elements
     newItem->size = size;
     newItem->next = my_stack->head;
     newItem->value = (void*)malloc(size);
 
+    // Copy user data into memory
     memcpy(newItem->value, data, size);
 
+    // Place at the top of the stack
     my_stack->head = newItem;
 
     return SUCCESS;
@@ -80,8 +88,10 @@ int stack_pop(stack* my_stack, void* data) {
     if (data == NULL)
         return UNDEF_DATA;
 
+    // Copy element data to user data
     memcpy(data, my_stack->head->value, my_stack->head->size);
 
+    // Remove the element from the stack, update stack to point to next element
     stack_ele* temp = my_stack->head;
     my_stack->head = my_stack->head->next;
     free(temp->value);
